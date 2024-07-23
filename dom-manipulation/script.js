6f00359c1294999958d5034241b46dda44d4d116
Emma_Tetteh <tettehe7@gmail.com>
2024-07-23 23:54:34 +0200

    new commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,6 +1,3 @@
-// script.js
-
-// Array of quote objects
 const quotes = [
     { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
@@ -46,6 +43,8 @@ const quotes = [
         quotes.push({ text: quoteText, category: quoteCategory });
         document.getElementById('formContainer').remove();
       }
+    //   Inserted new codes
+      ["createElement", "appendChild"]
     });
   }
   
